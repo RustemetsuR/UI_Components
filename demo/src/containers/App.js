059b649e6a00711d0d0c22db11b29d81d37e16d3
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-08-10 01:45:26 +0600

    Added <AllUicomponents/>

--- demo/src/containers/App.js
+++ demo/src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AllUiComponents from '../components/UI/AllUiComponents/AllUiComponents';
 
 const App = () =>{
   return (
     <div className="App">
-      
+      <AllUiComponents/>
     </div>
   );
 }
